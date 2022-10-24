57fde47203b3b7451b613c35f6d64db26e8a7cf2
Nick van den Dungen <nicktvdd@hotmail.com>
2022-10-24 19:04:04 +0300

    norminette

---
+++ Libft/ft_substr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nvan-den <nvan-den@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 18:38:03 by nvan-den          #+#    #+#             */
+/*   Updated: 2022/10/24 19:00:46 by nvan-den         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	int		i;
+
+	i = 0;
+	sub = malloc(sizeof(*s) * len + 1);
+	while (len)
+	{
+		sub[i] = s[i];
+		len--;
+		i++;
+	}
+	sub[i] = NULL;
+}
