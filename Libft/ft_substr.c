a9b201eb0687b445a1f8c6b3e0c30b143c2c0d40
Nick van den Dungen <nicktvdd@hotmail.com>
2022-10-24 19:06:13 +0300

    return null if malloc fails

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nvan-den <nvan-den@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 18:38:03 by nvan-den          #+#    #+#             */
-/*   Updated: 2022/10/24 19:00:46 by nvan-den         ###   ########.fr       */
+/*   Updated: 2022/10/24 19:06:09 by nvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	i = 0;
 	sub = malloc(sizeof(*s) * len + 1);
+	if (sub)
+	{
+		return (NULL);
+	}
 	while (len)
 	{
 		sub[i] = s[i];
