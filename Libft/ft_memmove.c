79004b4d800176193945995d5fd8ee2a7a5aa212
Nick van den Dungen <nicktvdd@hotmail.com>
2022-10-27 14:59:44 +0300

    norminette

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: nvan-den <nvan-den@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 15:33:02 by nvan-den          #+#    #+#             */
-/*   Updated: 2022/10/27 14:56:20 by nvan-den         ###   ########.fr       */
+/*   Updated: 2022/10/27 14:59:35 by nvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,18 @@ void	*memmove(void *dst, const void *src, size_t len)
 	i = 0;
 	d = (char *)dst;
 	s = (char *)src;
-	
 	if (d > s)
-		while (len--> 0)
+	{
+		while (len-- > 0)
 			d[len] = s[len];
+	}
 	else
+	{
 		while (i < len && s[i])
 		{
 			d[i] = s[i];
 			i++;
 		}
+	}
 	return (dst);
 }
