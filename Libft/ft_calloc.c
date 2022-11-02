584f77dff8ebe5d2d8c83b7372e27acd7f28bd9d
Nick van den Dungen <nicktvdd@hotmail.com>
2022-11-02 16:51:19 +0200

    testing

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,11 +6,10 @@
 /*   By: nvan-den <nvan-den@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 11:58:56 by nvan-den          #+#    #+#             */
-/*   Updated: 2022/10/31 16:54:43 by nvan-den         ###   ########.fr       */
+/*   Updated: 2022/11/02 16:51:19 by nvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
 
 void	*ft_calloc(size_t count, size_t size)
--- Libft/ft_memchr.c
+++ Libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: nvan-den <nvan-den@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 14:51:46 by nvan-den          #+#    #+#             */
-/*   Updated: 2022/11/02 15:06:03 by nvan-den         ###   ########.fr       */
+/*   Updated: 2022/11/02 16:51:19 by nvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	size_t			i;
 	unsigned char	*str;
 
-	str = (void *)s;
+	str = (unsigned char *)s;
 	i = 0;
 	while (i <= n)
 	{
