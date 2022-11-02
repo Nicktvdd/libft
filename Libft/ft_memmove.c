8be9b099fcb8544f41d8bb0653e1f67f534b669e
Nick van den Dungen <nicktvdd@hotmail.com>
2022-11-02 15:37:09 +0200

    memmove crashes on purpose

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: nvan-den <nvan-den@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 15:33:02 by nvan-den          #+#    #+#             */
-/*   Updated: 2022/11/02 15:06:22 by nvan-den         ###   ########.fr       */
+/*   Updated: 2022/11/02 15:36:03 by nvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	size_t	i;
-	char	*d;
-	char	*s;
+	size_t			i;
+	unsigned char	*d;
+	unsigned char	*s;
 
 	i = 0;
-	d = (char *)dst;
-	s = (char *)src;
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	if (dst == NULL && src == NULL)
+		return (dst);
 	if (d > s)
 	{
 		while (len-- > 0)
@@ -28,7 +30,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	}
 	else
 	{
-		while (i < len && s[i])
+		while (len-- > 0)
 		{
 			d[i] = s[i];
 			i++;
