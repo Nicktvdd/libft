07dd92b158bf3efcb6c34e4baca0bc2d99e6c737
Nick van den Dungen <nicktvdd@hotmail.com>
2022-11-03 13:14:28 +0200

    strlcpy working

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: nvan-den <nvan-den@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 15:00:51 by nvan-den          #+#    #+#             */
-/*   Updated: 2022/11/03 11:49:33 by nvan-den         ###   ########.fr       */
+/*   Updated: 2022/11/03 13:13:38 by nvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 
 	i = 0;
-	while (i < dstsize)
+	if (dstsize == 0)
+		return (ft_strlen(src));
+	while (i < dstsize - 1 && src[i])
 	{
 		dst[i] = src[i];
 		i++;
 	}
-	dst[--i] = '\0';
+	dst[i] = '\0';
 	return (ft_strlen(src));
 }
