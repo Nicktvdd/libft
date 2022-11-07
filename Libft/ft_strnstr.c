1aac862ea263762fa0a37b7c8bb52fa0386f2008
Nick van den Dungen <nicktvdd@hotmail.com>
2022-11-07 15:55:27 +0200

    strnstr warmachine compliant

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: nvan-den <nvan-den@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 18:53:08 by nvan-den          #+#    #+#             */
-/*   Updated: 2022/11/04 15:17:55 by nvan-den         ###   ########.fr       */
+/*   Updated: 2022/11/07 15:55:14 by nvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t		j;
 
 	i = 0;
-	if (*needle == '\0' || len == 0)
+	if (*needle == '\0')
 		return ((char *)haystack);
+	if (len == 0)
+		return (NULL);
 	while (haystack[i] && i < len)
 	{	
 		j = 0;
